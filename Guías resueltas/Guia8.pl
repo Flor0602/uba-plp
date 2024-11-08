20c2474403cd2f528624b5e4aa400060402a7268
Florencia Lin <131207512+Flor0602@users.noreply.github.com>
2024-11-07 22:34:13 -0300

    Update Guia8.pl

--- Guías resueltas/Guia8.pl
+++ Guías resueltas/Guia8.pl
@@ -25,4 +25,41 @@ ancestro(X, Y) :- ancestro(Z, Y), padre(X, Z).
 % vii. Explicar la respuesta a la consulta ancestro(juan, X). ¿Qué sucede si se pide más de un resultado?
 % viii. Sugerir un solución al problema hallado en los puntos anteriores reescribiendo el programa de ancestro.
 
+% Operaciones sobre listas
+% Ejercicio 4 - Definir el pred juntar(?L1,?L2,?L3), que tiene exito si L3 es la concatenación de L1 y L2. (Este pred ya está definido como append)
 
+juntar([],Ys,Ys).
+juntar([X|Xs],Ys,[X|Zs]) :- juntar(Xs,Ys,Zs).
+
+% Ejercicio 5 - Definir los predicados usando append.
+% last(?L,?U)
+last(L,U) :- append(_,[U],L).
+
+% reverse(+L,-L1)
+reverse([],[]).
+reverse([X|Xs],L1) :- reverse(Xs,S), append(S,[X],L1).
+
+% prefijo(?P, +L)
+prefijo(P,L) :- append(P,_,L).
+
+% sufijo(?S, +L)
+sufijo(S,L) :- append(_,S,L).
+
+% sublista(?S, +L)
+sublista(SL,L) :- sufijo(SL,R), prefijo(R,L).
+
+% pertenece(?X, +L)
+pertenece(X,L) :- sublista([X],L).
+
+/*
+Ejercicio 6 - Denir el predicado aplanar(+Xs, -Ys), que es verdadero sii Ys contiene los elementos de todos los niveles de
+Xs, en el mismo orden de aparición. Los elementos de Xs son enteros, átomos o nuevamente listas, de modo que
+Xs puede tener una profundidad arbitraria. Por el contrario, Ys es una lista de un solo nivel de profundidad.
+Ejemplos:
+?- aplanar([a, [3, b, []], [2]], L).→ L=[a, 3, b, 2]
+?- aplanar([[1, [2, 3], [a]], [[[]]]], L).→ L=[1, 2, 3, a]
+Nota: este predicado ya está denido en prolog con el nombre flatten.
+*/
+
+aplanar([],[]).
+aplanar().
